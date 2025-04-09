e8c8d2cd56790d1d9030040b07a5f2e8fe7c86da
itssonukumar <itssonukumar69@gmail.com>
2025-04-09 22:55:18 +0530

    commit

--- src/context/AppContext.jsx
+++ src/context/AppContext.jsx
@@ -1,4 +1,21 @@
-import { createContext } from "react";
+import { createContext, useContext,useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 export const AppContext=createContext();
 
+export const AppContextProvider=({children})=>{
+    const navigate=useNavigate();
+    const [user,setUser]=useState(null)
+    const [isSeller,setIsSeller]=useState(null)
+    const [showUserLogin,setShowUserLogin]=useState(null)
+
+
+    const value={navigate,user,setUser,setIsSeller,isSeller,showUserLogin,setShowUserLogin}
+return<AppContext.Provider value={value}>
+    {children}
+</AppContext.Provider>
+}
+
+export const useAppContext=()=>{
+    return useContext(AppContext)
+}
