af7f699be83e22574d12be9ac0582d3b430e3678
itssonukumar <itssonukumar69@gmail.com>
2025-05-01 12:32:16 +0530

    commit

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -29,7 +29,7 @@ const ProductCard = ({product}) => {
                 </div>
                 <div className="flex items-end justify-between mt-3">
                     <p className="md:text-xl text-base font-medium text-bg-primary">
-                        {currency}${product.offerPrice} {' '} <span className="text-gray-500/60 md:text-sm text-xs line-through">{currency}${product.price}</span>
+                        {currency}{product.offerPrice} {' '} <span className="text-gray-500/60 md:text-sm text-xs line-through">{currency}{product.price}</span>
                     </p>
                     <div onClick={(e)=>e.stopPropagation()} className="text-bg-primary">
                         {!cartItems[product._id] ? (
